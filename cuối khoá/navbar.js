cc7b880522b32e31b969ef0b8b4d5ccaafc66f9c
bybooxyg <caotung230911@gmail.com>
2026-02-27 06:19:52 +0700

    Add files via upload

---
+++ cuối khoá/navbar.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+
+  if (!currentUser) {
+    window.location.href = "login.html";
+    return;
+  }
+
+  const userName = document.getElementById("userName");
+  const avatarBox = document.getElementById("userAvatar");
+  const userBox = document.getElementById("userBox");
+
+  if (!userName || !avatarBox || !userBox) return;
+
+  userName.innerText = currentUser.username;
+
+  if (currentUser.avatar && currentUser.avatar.trim() !== "") {
+    avatarBox.innerHTML = `<img src="${currentUser.avatar}" alt="avatar">`;
+  } else {
+    avatarBox.innerText = currentUser.username.charAt(0).toUpperCase();
+  }
+
+  userBox.addEventListener("click", () => {
+    window.location.href = "profile.html";
+  });
+
+});
---
+++ cuối khoá/post_sell.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tiny.cloud/1/mmoz8hxafxvd2ndd0r499a51ycfk9roianwjgort8mqaos1m/tinymce/8/tinymce.min.js" referrerpolicy="origin" crossorigin="anonymous"></script>
+</head>
+<body>
+    <script>
+  tinymce.init({
+    selector: 'textarea',
+    plugins: [
+      // Core editing features
+      'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'link', 'lists', 'media', 'searchreplace', 'table', 'visualblocks', 'wordcount',
+      // Your account includes a free trial of TinyMCE premium features
+      // Try the most popular premium features until Feb 23, 2026:
+      'checklist', 'mediaembed', 'casechange', 'formatpainter', 'pageembed', 'a11ychecker', 'tinymcespellchecker', 'permanentpen', 'powerpaste', 'advtable', 'advcode', 'advtemplate', 'ai', 'uploadcare', 'mentions', 'tinycomments', 'tableofcontents', 'footnotes', 'mergetags', 'autocorrect', 'typography', 'inlinecss', 'markdown','importword', 'exportword', 'exportpdf'
+    ],
+    toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography uploadcare | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+    tinycomments_mode: 'embedded',
+    tinycomments_author: 'Author name',
+    mergetags_list: [
+      { value: 'First.Name', title: 'First Name' },
+      { value: 'Email', title: 'Email' },
+    ],
+    ai_request: (request, respondWith) => respondWith.string(() => Promise.reject('See docs to implement AI Assistant')),
+    uploadcare_public_key: '1e1d4bfe7bb4f7db8a73',
+  });
+</script>
+<form action="
+"><textarea name="" id="textarea"></textarea></form>
+</body>
+</html>
