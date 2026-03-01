c0532ab82d9487968f53ea1f04cb45568a05aabd
Tuan Anh <phantuananh0202@gmail.com>
2026-03-01 16:28:46 +0700

    fix: navbar

--- cuối khoá/navbar.js
+++ cuối khoá/navbar.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-
   const currentUser = JSON.parse(localStorage.getItem("currentUser"));
 
   if (!currentUser) {
@@ -24,5 +23,31 @@ document.addEventListener("DOMContentLoaded", function () {
   userBox.addEventListener("click", () => {
     window.location.href = "profile.html";
   });
-
 });
+
+function initUser() {
+  const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+
+  if (!currentUser) {
+    window.location.href = "login.html";
+    return;
+  }
+
+  const userName = document.getElementById("userName");
+  const avatarBox = document.getElementById("userAvatar");
+  const userBox = document.getElementById("userBox");
+
+  if (!userName || !avatarBox || !userBox) return;
+
+  userName.innerText = currentUser.username;
+
+  if (currentUser.avatar && currentUser.avatar.trim() !== "") {
+    avatarBox.innerHTML = `<img src="${currentUser.avatar}">`;
+  } else {
+    avatarBox.innerText = currentUser.username.charAt(0).toUpperCase();
+  }
+
+  userBox.addEventListener("click", () => {
+    window.location.href = "profile.html";
+  });
+}
